ea1de6445deff99998b6a26c01ea7e63db12b858
larry.adams <ladams1776@yahoo.com>
2019-09-21 18:38:29 -0400

    Add the Limit Service
    From Repo: https://github.com/ladams1776/limits-service

---
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitConfiguration.java
@@ -0,0 +1,30 @@
+package com.microservices.limitsservice;
+
+public class LimitConfiguration {
+    private int maximum;
+    private int minimum;
+
+    public LimitConfiguration() {
+    }
+
+    public LimitConfiguration(int minimum, int maximum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+}
