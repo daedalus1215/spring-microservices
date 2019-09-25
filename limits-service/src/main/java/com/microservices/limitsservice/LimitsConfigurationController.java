0a90270e373f6ce69dbad9acd51977a8782d7946
larry.adams <ladams1776@yahoo.com>
2019-09-24 21:41:26 -0400

    Enable Fault Tolerance on the LimitService

--- limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,18 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(this.configuration.getMinimum(), this.configuration.getMaximum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        // in the real world there would be functionality here
+        // and if there is an issue, like an exception thrown
+        // we will hit the fallbackRetrieveConfiguration RequestHandling method
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(9, 999);
     }
 }
--- limits-service/src/main/java/com/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,9 +3,11 @@ package com.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix // This will enable hystrix fault tolerance on all the controllers
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
